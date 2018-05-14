3c5932b25cb615a0b0a039d32d947359d70b42a6
Dhruv <dhruvdev7794@gmail.com>
2018-05-14 10:44:23 -0400

    implemented findAll by seperating controller and models

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,19 +1,70 @@
 (function(){
-    var $usernameFld, $passwordFld;
-    var $removeBtn, $editBtn, $createBtn;
-    var $firstNameFld, $lastNameFld;
-    var $userRowTemplate, $tbody;
-    var userService = new AdminUserServiceClient();
 
-    $(main);
+    jQuery(main);
 
-    function main(){}
-    function createUser(){}
-    function findAllUsers(){}
-    function findUserById(){}
-    function deleteUser(){}
-    function selectUser(){}
-    function updateUser(){}
-    function renderUser(user){}
-    function renderUsers(users){}
+    var tbody;
+    var tr_template;
+    var userService = new UserServiceClient();
+    function main(){
+        // hide the template
+        // var hidden = $('.wbdv-hidden');
+        // hidden.css('display', 'none')
+
+        tr_template = $('.wbdv-template');
+        tbody = $('tbody');
+        findAllUsers();
+    }
+
+    function findAllUsers() {
+        userService.findAllUsers().then(function (response) {
+            return response.json();
+        }).then(renderUsers);
+    }
+
+
+    function renderUsers(users){
+        // render all the users using the template
+        for(var i = 0 ; i < users.length ; i++){
+            var user = users[i];
+            var clone  = tr_template.clone();
+            clone.find('.wbdv-username').html(user.username);
+            clone.find('.wbdv-first-name').html(user.firstName);
+            clone.find('.wbdv-last-name').html(user.lastName);
+            clone.find('.wbdv-phone').html(user.phone);
+            clone.find('.wbdv-email-id').html(user.email);
+            clone.find('.wbdv-role').html(user.role);
+            tbody.append(clone);
+        }
+    }
+
+
+
+
+    // function deleteUser(event){
+    //
+    // }
+
+
+
+
+
+
+
+
+//    var $usernameFld, $passwordFld;
+//    var $removeBtn, $editBtn, $createBtn;
+//    var $firstNameFld, $lastNameFld;
+//    var $userRowTemplate, $tbody;
+//    var userService = new AdminUserServiceClient();
+//    $(main);
+//
+//    function main(){}
+//    function createUser(){}
+//    function findAllUsers(){}
+//    function findUserById(){}
+
+//    function selectUser(){}
+//    function updateUser(){}
+//    function renderUser(user){}
+//    function renderUsers(users){}
 })();
--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -1,14 +1,23 @@
 function UserServiceClient() {
-    this.createUser = createUser;
-    this.findAllUsers = findAllUsers;
-    this.findUserById = findUserById;
-    this.deleteUser = deleteUser;
-    this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
-    var self = this;
-    function createUser(user, callback) {}
-    function findAllUsers(callback) {}
+	// this.createUser = createUser;
+	this.findAllUsers = findAllUsers;
+	// this.findUserById = findUserById;
+	// this.deleteUser = deleteUser;
+	// this.updateUser = updateUser;
+	this.url = 'http://localhost:8080/api/user';
+	var self = this;
+
+
+	function findAllUsers(){
+        return fetch('http://localhost:8080/api/user');
+        // return promise.then(function (response){
+        //     return response.json();
+        // }).then(renderUsers)
+        //     .then();
+	}
     function findUserById(userId, callback) {}
+    function deleteUser(userId) {}
     function updateUser(userId, user, callback) {}
-    function deleteUser(userId, callback) {}
+
+
 }
