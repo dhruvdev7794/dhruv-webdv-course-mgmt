aee4dab652ae3de43f3cbac79cb55d84790ef3b7
Dhruv <dhruvdev7794@gmail.com>
2018-05-16 17:43:14 -0400

    added logout (but not used)

--- src/main/java/com/example/dhruvsharmacoursemgmt/services/UserService.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.dhruvsharmacoursemgmt.services;
 import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -125,4 +126,10 @@ public class UserService {
 	public Iterable<User> findUserByUsername(@RequestBody String username){
 		return userRepository.findUserByUsername(username);
 	}
+	
+	@PostMapping("/api/logout")
+	public void logout (HttpSession session) {
+		session.invalidate();
+	}
+
 }
