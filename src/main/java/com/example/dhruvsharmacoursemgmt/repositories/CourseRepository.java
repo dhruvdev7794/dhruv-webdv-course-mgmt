61af7e11709fd5ccdb3661a8c773ceb8f89b9971
Dhruv <dhruvdev7794@gmail.com>
2018-05-19 11:51:58 -0400

    added course repository

---
+++ src/main/java/com/example/dhruvsharmacoursemgmt/repositories/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.example.dhruvsharmacoursemgmt.repositories;
+import org.springframework.data.repository.CrudRepository;
+import com.example.dhruvsharmacoursemgmt.model.Course;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+}
