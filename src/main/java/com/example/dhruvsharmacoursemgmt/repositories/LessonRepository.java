c8c1c10cf08c9c2ca817d811d1a09ce815f25e26
Dhruv <dhruvdev7794@gmail.com>
2018-05-20 17:25:54 -0400

    added lesson repository

---
+++ src/main/java/com/example/dhruvsharmacoursemgmt/repositories/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.example.dhruvsharmacoursemgmt.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.dhruvsharmacoursemgmt.model.Module;
+
+public interface LessonRepository extends CrudRepository<Module, Integer>{
+
+}
