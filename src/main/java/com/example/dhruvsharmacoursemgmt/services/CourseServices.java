513ffd1f56b2d111cd6b597dd0668c91f735d082
Dhruv <dhruvdev7794@gmail.com>
2018-05-20 16:11:24 -0400

    added services for course and modules

--- src/main/java/com/example/dhruvsharmacoursemgmt/services/CourseServices.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/CourseServices.java
@@ -1,15 +1,19 @@
 package com.example.dhruvsharmacoursemgmt.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dhruvsharmacoursemgmt.model.Course;
+import com.example.dhruvsharmacoursemgmt.model.User;
 import com.example.dhruvsharmacoursemgmt.repositories.CourseRepository;
 
 @RestController
@@ -31,6 +35,34 @@ public class CourseServices {
 	@DeleteMapping("/api/course/{courseId}")
 	public void deleteCourse(@PathVariable("courseId") int id) {
 		courseRepository.deleteById(id);
+	}
+	
+	@PutMapping("/api/course/{userId}")
+	public Course updateCourse(@PathVariable("courseId") int courseId, @RequestBody Course newCourse) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+//			Course course = data.get();
+//			course.setFirstName(newUser.getFirstName());
+//			course.setLastName(newUser.getLastName());
+//			course.setEmail(newUser.getEmail());
+//			course.setPhone(newUser.getPhone());
+//			course.setRole(newUser.getRole());
+//			course.setUsername(newUser.getUsername());
+//			course.setDateOfBirth(newUser.getDateOfBirth());
+			// do it for the rest of the fields
+			courseRepository.save(newCourse);
+			return newCourse;
+		}
+		return null;
+	}
+	
+	@GetMapping("/api/course/{courseId}")
+	public Course findCourseById(@PathVariable("courseId") int courseId) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		return null;
 	}
 
 
--- src/main/java/com/example/dhruvsharmacoursemgmt/services/ModuleService.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/ModuleService.java
@@ -1,7 +1,10 @@
 package com.example.dhruvsharmacoursemgmt.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +13,18 @@ import com.example.dhruvsharmacoursemgmt.model.Module;
 import com.example.dhruvsharmacoursemgmt.repositories.CourseRepository;
 import com.example.dhruvsharmacoursemgmt.repositories.ModuleRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ModuleService {
 	@Autowired
 	CourseRepository courseRepository;
 	@Autowired
 	ModuleRepository moduleRepository;
 	
+	@PostMapping("/api/course/{courseId}/module")
 	public Module createModule(
 			@PathVariable("courseId") int courseId,
 			@RequestBody Module newModule) {
@@ -27,6 +33,17 @@ public class ModuleService {
 			Course course = data.get();
 			newModule.setCourse(course);
 			return moduleRepository.save(newModule);
+		}
+		return null;
+	}
+	
+	
+	@GetMapping("/api/course/{courseId}/module")
+	public List<Module> findAllModulesForCourse(@PathVariable("courseId") int courseId){
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			return course.getModules();
 		}
 		return null;
 	}
