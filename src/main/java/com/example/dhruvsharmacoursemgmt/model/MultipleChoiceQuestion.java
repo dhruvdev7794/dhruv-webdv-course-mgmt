feeeb0c4ea83dbef28c68c4e125269f496467df4
Dhruv <dhruvdev7794@gmail.com>
2018-06-28 09:53:10 -0400

    modified model

--- src/main/java/com/example/dhruvsharmacoursemgmt/model/MultipleChoiceQuestion.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/model/MultipleChoiceQuestion.java
@@ -1,5 +1,20 @@
 package com.example.dhruvsharmacoursemgmt.model;
 
-public class MultipleChoiceQuestion {
-
+public class MultipleChoiceQuestion extends Question{
+	private String options;
+	private int correctOption;
+	
+	
+	public String getOptions() {
+		return options;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	public int getCorrectOption() {
+		return correctOption;
+	}
+	public void setCorrectOption(int correctOption) {
+		this.correctOption = correctOption;
+	}
 }
