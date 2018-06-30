c86950cb2e7480bb92a42bfe54a0a729bb061305
Dhruv <dhruvdev7794@gmail.com>
2018-06-29 22:50:25 -0400

    minor changes

--- src/main/java/com/example/dhruvsharmacoursemgmt/repositories/MultipleChoiceQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dhruvsharmacoursemgmt.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.dhruvsharmacoursemgmt.model.MultipleChoiceQuestion;
-
-public interface MultipleChoiceQuestionRepository extends CrudRepository<MultipleChoiceQuestion, Integer>{
-
-}
--- src/main/java/com/example/dhruvsharmacoursemgmt/repositories/TrueFalseQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dhruvsharmacoursemgmt.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.dhruvsharmacoursemgmt.model.TrueFalseQuestion;
-
-public interface TrueFalseQuestionRepository extends CrudRepository<TrueFalseQuestion, Integer>{
-
-}
