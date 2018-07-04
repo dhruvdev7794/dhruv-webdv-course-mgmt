6bc9d6ea36f16953cf596cee94116da4db2ebb3d
Dhruv <dhruvdev7794@gmail.com>
2018-07-05 00:38:26 +0530

    made service file for exam and modified assignment

--- src/main/java/com/example/dhruvsharmacoursemgmt/services/AssignmentService.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/AssignmentService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,14 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dhruvsharmacoursemgmt.model.Assignment;
-import com.example.dhruvsharmacoursemgmt.model.Course;
 import com.example.dhruvsharmacoursemgmt.model.Lesson;
 import com.example.dhruvsharmacoursemgmt.model.Widget;
 import com.example.dhruvsharmacoursemgmt.repositories.AssignmentRepository;
 import com.example.dhruvsharmacoursemgmt.repositories.LessonRepository;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="*", maxAge=3600)
 public class AssignmentService {
 	@Autowired
 	AssignmentRepository assgnRepo;
@@ -31,7 +32,7 @@ public class AssignmentService {
 	
 	@DeleteMapping("api/assignment/{assignmentId}")
 	public void deleteAssignment(@PathVariable("assignmentId") int assignmentId) {
-		assgnRepo.deleteById(assignmentId);
+		assgnRepo.deleteById(assignmentId);	
 	}
 	
 	@PutMapping("api/lesson/{lessonId}/assignment")
@@ -61,29 +62,30 @@ public class AssignmentService {
 	}
 	
 	@GetMapping("api/lesson/{lessonId}/assignment")
-	public Iterable<Assignment> findAssignmentsForLesson(@PathVariable("lessonId") int lessonId){
+	public Iterable<Assignment> findAssignmentsForLesson(@PathVariable("lessonId") int lessonId, HttpServletResponse response){
 		Optional<Lesson> lesson = lessonRepo.findById(lessonId);
 		if(lesson.isPresent()) {
 			Lesson newLesson = lesson.get();
 			List<Widget> widgets = newLesson.getWidgets();
 			List<Assignment> assignList = new ArrayList();
 			for(Widget widget: widgets) {
-				if(widget.getWidgetType()=="Assignment") {
+				if(widget.getWidgetType().equals("Assignment")) {
 					assignList.add((Assignment) widget);
 				}
 			}
-			
 			return assignList;
 		}
+		response.setStatus(HttpServletResponse.SC_CONFLICT);
 		return null;
 	}
 	
-	@GetMapping("api/assignment/{assignId}")
-	public Assignment getAssignmentFromId(@PathVariable("assignmentId") int assignmentId) {
+	@GetMapping("api/assignment/{assignmentId}")
+	public Assignment getAssignmentFromId(@PathVariable("assignmentId") int assignmentId, HttpServletResponse response) {
 		Optional<Assignment> assignment = assgnRepo.findById(assignmentId);
 		if(assignment.isPresent()) {
 			return assignment.get();
 		}
+		response.setStatus(HttpServletResponse.SC_CONFLICT);
 		return null;
 	}
 	
---
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/ExamService.java
@@ -0,0 +1,63 @@
+package com.example.dhruvsharmacoursemgmt.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dhruvsharmacoursemgmt.model.Assignment;
+import com.example.dhruvsharmacoursemgmt.model.Exam;
+import com.example.dhruvsharmacoursemgmt.model.Lesson;
+import com.example.dhruvsharmacoursemgmt.model.Widget;
+import com.example.dhruvsharmacoursemgmt.repositories.ExamRepository;
+import com.example.dhruvsharmacoursemgmt.repositories.LessonRepository;
+
+
+@RestController
+@CrossOrigin(origins="*", maxAge=3600)
+public class ExamService {
+	@Autowired
+	ExamRepository examRepo;
+	
+	@Autowired
+	LessonRepository lessonRepo;
+	
+	@GetMapping("api/exam")
+	public Iterable<Exam> findAllExams(){
+		return examRepo.findAll();
+	}
+	
+	@GetMapping("api/lesson/{examId}/exam")
+	public Iterable<Exam> findExamForLesson(@PathVariable("lessonId") int lessonId){
+		Optional<Lesson> lesson = lessonRepo.findById(lessonId);
+		if(lesson.isPresent()) {
+			Lesson newLesson = lesson.get();
+			List<Widget> widgets = newLesson.getWidgets();
+			List<Exam> examList = new ArrayList();
+			for(Widget widget: widgets) {
+				if(widget.getWidgetType().equals("Exam")) {
+					examList.add((Exam) widget);
+				}
+			}
+			return examList;
+		}
+		return null;
+	}
+	
+	@GetMapping("api/exam/{examId}")
+	public Exam getAssignmentFromId(@PathVariable("examId") int examId, HttpServletResponse response) {
+		Optional<Exam> exam = examRepo.findById(examId);
+		if(exam.isPresent()) {
+			return exam.get();
+		}
+		response.setStatus(HttpServletResponse.SC_CONFLICT);
+		return null;
+	}
+}
