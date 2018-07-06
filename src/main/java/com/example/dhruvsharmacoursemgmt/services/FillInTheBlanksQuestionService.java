d38748f2cd60de9b72cdec193d1a6643a1e6110f
Dhruv <dhruvdev7794@gmail.com>
2018-07-06 09:02:05 +0530

    fill services

--- src/main/java/com/example/dhruvsharmacoursemgmt/services/FillInTheBlanksQuestionService.java
+++ src/main/java/com/example/dhruvsharmacoursemgmt/services/FillInTheBlanksQuestionService.java
@@ -2,16 +2,21 @@ package com.example.dhruvsharmacoursemgmt.services;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dhruvsharmacoursemgmt.repositories.FillInTheBlanksRepository;
 import com.example.dhruvsharmacoursemgmt.model.Exam;
 import com.example.dhruvsharmacoursemgmt.model.FillInTheBlanksQuestion;
+import com.example.dhruvsharmacoursemgmt.model.MultipleChoiceQuestion;
 import com.example.dhruvsharmacoursemgmt.repositories.ExamRepository;
 
 @RestController
@@ -34,5 +39,28 @@ public class FillInTheBlanksQuestionService {
 		}
 		return null;
 	}
+
+	@PutMapping("api/exam/{examId}/blanks")
+	public FillInTheBlanksQuestion updateFBQuestion(@PathVariable("examId") int examId,
+									@RequestBody FillInTheBlanksQuestion question) {
+		Optional<Exam> exam = examRepo.findById(examId);
+		if(exam.isPresent()) {
+			Exam newExam = exam.get();
+			question.setExam(newExam);
+			return fillInBlanksRepo.save(question);
+		}
+		return null;
+	}
+	
+	@GetMapping("api/blanks/{questionId}")
+	public FillInTheBlanksQuestion findFBFromQuestionId(@PathVariable("questionId") int questionId, HttpServletResponse response) {
+		Optional<FillInTheBlanksQuestion> fbObj = fillInBlanksRepo.findById(questionId);
+		if(fbObj.isPresent()) {
+			return fbObj.get();
+		}
+		response.setStatus(HttpServletResponse.SC_CONFLICT);
+		return null;
+	}
+
 
 }
